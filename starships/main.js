88a7d622d129bf790a122d324211a5da2ec83209
Seth Elliott <sethfe617@gmail.com>
2020-03-19 10:38:17 -0600

    added modal from bulma

---
+++ starships/main.js
@@ -0,0 +1,60 @@
+import { starships } from '../data/starships.js'
+import {removeChildren} from '../utils.js' 
+
+
+
+const nav = document.querySelector('.nav')
+
+const navList = document.querySelector('.navList')
+
+const shipView = document.querySelector('#main')
+
+const modalDialog = document.querySelector('.modal')
+const modalButton = document.querySelector('.modal-close')
+const modalBackground =document.querySelector('.modal-background')
+const modalMessage = document.querySelector('.dialogMessage')
+
+modalButton.addEventListener('click', () => {
+  modalDialog.classList.toggle("is-active")
+})
+
+function populateNav(starships) {
+    starships.forEach(starship => {
+      let shipAnchor = document.createElement('a')
+      shipAnchor.href = '#' 
+      let listItem = document.createElement('li')
+      listItem.textContent = starship.name
+
+      shipAnchor.addEventListener('click', event => {
+        //store the name of the item clicked on 
+          let shipName = event.target.textContent
+          const foundShip = starships.find(ship =>
+            ship.name === shipName )
+            console.log(doundship)
+          populateShipView(foundShip)
+      })
+
+
+      shipAnchor.appendChild(listItem)
+      navList.appendChild(shipAnchor)
+
+    })
+    nav.appendChild(navList)
+}
+
+function populateShipView(shipData) {
+    removeChildren(shipView)
+    let imageNum = getLastNumber(shipData.url)
+    let shipImage = document.createElement('img')
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${imageNum}.jpg`
+    
+    shipImage.addEventListener('error', event => {
+      shipImage.hidden = truemodalDialog.classList.toggle("is-active")
+      modalMessage.textContent = `Sorry, could not find an image for ${shipData.name}.`
+    })
+
+    shipView.appendChild(shipImage)
+}
+
+populateNav(starships)
+    
