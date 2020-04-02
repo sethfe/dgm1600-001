71d024ee24eb32468cc7e80a1a3ff27861959dda
Seth Elliott <sethfe617@gmail.com>
2020-04-02 11:48:46 -0600

    senators

---
+++ Congress/index.js
@@ -0,0 +1,51 @@
+import { senators } from '..data/senators.js'
+
+
+
+const container = document.querySelector('.container')
+
+const filterSenators = (prop, value) => {
+    return senators.filter(senator => senator[prop] === value)
+}
+
+function simplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+        return { 
+            id: senator.id,
+            name: `${senator.first_name}${middleName}${senator.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10)
+        }
+    })
+}
+
+function populateContainer(smallSenatorArray) {
+    return smallSenatorArray.forEach(senator => {
+
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        container.appendChild(senFigure)
+    })
+}
+
+const republicans = filterSenators('party', 'R')
+const democrats = filterSenators('party', 'D')
+
+const mostSeniority = simplifiedSenators(republicans).reduce(
+    (acc, senator) => {
+        return acc.seniority > senator.seniority ? acc : senator
+    }
+ )
+
+ console.log(mostSeniority)
+
+ populateContainer(simplifiedSenators(republicans))
+
