78bea937e2db2481f3ba018b09ad17d1fc43c08f
Seth Elliott <sethfe617@gmail.com>
2020-04-14 12:04:26 -0600

    pokemon changes

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,15 +1,13 @@
 let pokeContainer = document.querySelector('.pokeContainer')
+let startButton = document.querySelector('#startButton')
+startButton.addEventListener('click', () => {
+  loadPage()
+})
 
-/* function getPokeData(url) {
-  fetch(url).then(function (response) {
-    response.json().then(function (pokemon) {
-      console.log(pokemon.results)
-      populatePokeCards(pokemon.results)
-    })
-  })
-}
-getPokeData('https://pokeapi.co/api/v2/pokemon') */
-// https://pokeapi.co/api/v2/pokemon/1/
+let newButton = document.querySelector('#newButton')
+newButton.addEventListener('click', () => {
+  addPokemon()
+})
 
 async function getAPIData(url) {
   try {
@@ -22,17 +20,19 @@ async function getAPIData(url) {
   }
 }
 
-getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
-  (data) => {
-    for (const pokemon of data.results) {
-      getAPIData(pokemon.url).then(
-        (pokeData) => {
-          populatePokeCard(pokeData)
-        }
-      )
+function loadPage() {
+  getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+    (data) => {
+      for (const pokemon of data.results) {
+        getAPIData(pokemon.url).then(
+          (pokeData) => {
+            populatePokeCard(pokeData)
+          }
+        )
+      }
     }
-  }
-)
+  )
+}
 
 function populatePokeCard(singlePokemon) {
     let pokeScene = document.createElement('div')
@@ -51,33 +51,49 @@ function populatePokeCard(singlePokemon) {
     pokeContainer.appendChild(pokeScene)
 }
 
-function populateCardFront(pokeMon) {
+function populateCardFront(pokemon) {
   let cardFront = document.createElement('div')
   cardFront.className = 'card__face card__face--front'
-  cardFront.textContent = `${pokeMon.name} ${pokeMon.id}`
+  
   let frontImage = document.createElement('img')
-  frontImage.src = '../images/001.png'
+  frontImage.src = `../images/${getImageFileName(pokemon)}.png`
+  let frontLabel = document.createElement('p') 
+  frontLabel.textContent = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`
   cardFront.appendChild(frontImage)
+  cardFront.appendChild(frontLabel)
   return cardFront
 }
 
-function populateCardBack(pokeMon) {
+function getImageFileName(pokemon) {
+  if (pokemon.id < 10) {
+    return `00${pokemon.id}`
+  } else if (pokemon.id > 9 && pokemon.id < 100) {
+    return `0${pokemon.id}`
+  } else return `pokeball`
+}
+
+function populateCardBack(pokemon) {
   let cardBack = document.createElement('div')
   cardBack.className = 'card__face card__face--back'
-  cardBack.textContent = pokeMon.stats[0].stat.name
+  cardBack.textContent = pokemon.stats[0].stat.name
   return cardBack
 }
 
-{
-  /* <div class="scene">
-  <div class="card">
-    <div class="card__face card__face--front">front</div>
-    <div class="card__face card__face--back"><div><p>Hi,I'm here on the back</p></div></div>
-  </div>
-</div>
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
-*/
+class Pokemon {
+  constructor(height, weight, name, stats) {
+    this.height = height
+    this.weight = weight
+    this.name = name
+    this.stats = stats
+    this.id = 900
+  }
+}
+
+function addPokemon() {
+  let newPokemon = new Pokemon(50, 25, 'Thoremon', [
+    {
+      stat:
+        { name: 'Thunder Belly' }
+    }])
+  populatePokeCard(newPokemon)
 }
